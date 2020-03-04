9ca35c437467a86f5c27a17248aaeb993a5a8a1c
qianfeiying <qianfeiying@yaoyouke.com>
2020-03-04 17:40:01 +0800

    11

---
+++ PBTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PBTest
+//
+//  Created by KnowChat03 on 2020/3/4.
+//  Copyright © 2020 KnowChat03. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+use_frameworks!
+
+target ‘PBTest’ do
+#   pod 'Protobuf', '~> 3.11.4'
+   #pod 'ProtocolBuffers', '~> 1.9.11'
+end
